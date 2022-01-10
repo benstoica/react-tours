e291593756fdad3fc927f214538fa306c099649a
Ben Stoica <bstoica@uwm.com>
2022-01-10 08:54:13 -0500

    setting up fetch data

--- src/App.js
+++ src/App.js
@@ -5,7 +5,39 @@ import Tours from "./Tours";
 const url = "https://course-api.com/react-tours-project";
 
 function App() {
-  return <h2>Project Setup</h2>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App;
